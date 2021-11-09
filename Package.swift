cbd4cfd089e73f4119a77ec290ec7d413b610252
Distiller <distiller@static.38.39.186.66.cyberlynk.net>
2021-11-09 17:05:29 +0000

    Update to SDK 4.2.1

--- Package.swift
+++ Package.swift
@@ -21,14 +21,14 @@ let package = Package(
         .binaryTarget(
             name: "GSSDKCore",
             url: "https://s3.amazonaws.com/tgl.geniusscan.sdk/GSSDKCore-4.2.1.xcframework.zip",
-            checksum: "b324e412c2a2558f25a183994354ee3d0f6bd8a893230e16bcaf1cefb2b05d8a"),
+            checksum: "e1d2aa0d9d92d18d7690e95dae3c9391f0c7657ea49dad88ed633f6b3fcf6be3"),
         .binaryTarget(
             name: "GSSDKScanFlow",
             url: "https://s3.amazonaws.com/tgl.geniusscan.sdk/GSSDKScanFlow-4.2.1.xcframework.zip",
-            checksum: "f664bad76c5ffcc353337ab065bc9d04aa52e60fbb8c40664b9f0171db238fa6"),
+            checksum: "3ee707dede85d611e5f8a1b756bdef9777607d92112cc4bf3c4153efae10e19a"),
         .binaryTarget(
             name: "GSSDKOCR",
             url: "https://s3.amazonaws.com/tgl.geniusscan.sdk/GSSDKOCR-4.2.1.xcframework.zip",
-            checksum: "f3c2fc9ddde01399340bdc9d1e84a471db87703b752f20acc2698b0b00856c07"),
+            checksum: "9c1cebe7cf261081f8f482ebc83223a5e8e941c941f71968e8404cfd1e0aad7d"),
     ]
 )
