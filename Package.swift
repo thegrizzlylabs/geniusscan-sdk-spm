f0d8c267f655f6482f18ff7764f2aec942650c29
The Grizzly Labs <sdk@thegrizzlylabs.com>
2024-02-09 18:31:24 +0000

    Update to SDK 5.0.0-beta4

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "GSSDK",
-            url: "https://s3.amazonaws.com/tgl.geniusscan.sdk/GSSDK-5.0.0-beta3.xcframework.zip",
-            checksum: "3b90583abc736eafb8610ff2f9bab0830e13fb100f8a14266ea2f85f183e5407")
+            url: "https://s3.amazonaws.com/tgl.geniusscan.sdk/GSSDK-5.0.0-beta4.xcframework.zip",
+            checksum: "0e49ce5d180c39328871bdf2938422dd46d06efe0518cfa7efbf47e4cc707320")
     ]
 )
