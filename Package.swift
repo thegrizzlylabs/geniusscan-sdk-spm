98511efc467c809b7f61a6e05b813e1d316a0677
The Grizzly Labs <sdk@thegrizzlylabs.com>
2024-02-13 14:41:42 +0000

    Update to SDK 5.0.0-beta5

--- Package.swift
+++ Package.swift
@@ -5,30 +5,14 @@ let package = Package(
     name: "Genius Scan SDK",
     products: [
         .library(
-            name: "GSSDKCore",
-            targets: ["GSSDKCore"]
-        ),
-        .library(
-            name: "GSSDKScanFlow",
-            targets: ["GSSDKScanFlow"]
-        ),
-        .library(
-            name: "GSSDKOCR",
-            targets: ["GSSDKOCR"]
+            name: "GSSDK",
+            targets: ["GSSDK"]
         )
     ],
     targets: [
         .binaryTarget(
-            name: "GSSDKCore",
-            url: "https://s3.amazonaws.com/tgl.geniusscan.sdk/GSSDKCore-4.21.1.xcframework.zip",
-            checksum: "64c15fbc24c623a42d243e81a99d952cd8b0344e82725ba9507f1380d82c1866"),
-        .binaryTarget(
-            name: "GSSDKScanFlow",
-            url: "https://s3.amazonaws.com/tgl.geniusscan.sdk/GSSDKScanFlow-4.21.1.xcframework.zip",
-            checksum: "2467182247ef30bbcd3ea516b7e7be60bd62b3997c25c083a103d265747373f5"),
-        .binaryTarget(
-            name: "GSSDKOCR",
-            url: "https://s3.amazonaws.com/tgl.geniusscan.sdk/GSSDKOCR-4.21.1.xcframework.zip",
-            checksum: "18732f8ed0770149ccd31a3d0878e70b31afe5612a92b42893046e962a36c281"),
+            name: "GSSDK",
+            url: "https://s3.amazonaws.com/tgl.geniusscan.sdk/GSSDK-5.0.0-beta5.xcframework.zip",
+            checksum: "9c4e5ac2b09b340e01cf83d9098fe63bc61e756868050c99a8424cbf59405f3a")
     ]
 )
